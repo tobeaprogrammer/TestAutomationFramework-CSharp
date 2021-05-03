8ea8673bbda6879ff811cb51f5fc71f9a34ca551
Saurabh Kulkarni <saurabh@Saurabhs-MacBook-Pro.local>
2021-05-04 01:14:58 +0530

    Setting Up the Repo

---
+++ TestAutomationFramework_CWS/TAF_Util/BaseDriver.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Internal;
+using OpenQA.Selenium.Support.UI;
+
+namespace TestAutomationFramework_CWS.TAF_Util
+{
+    public class BaseDriver : IWebDriver, IWrapsDriver
+    {
+        private static BaseDriver _instance = new BaseDriver();
+        public static TimeSpan ExplicitTimeOut = TimeSpan.FromSeconds(int.Parse(ConfigurationManager.AppSettings["DefaultExplicitWait"]));
+        public static TimeSpan defaultExplicitWait = TimeSpan.FromSeconds(int.Parse(ConfigurationManager.AppSettings["DefaultExplicitWait"]));
+
+        // To Initialize the browser
+        public BaseDriver()
+        {
+            var defaultPageLoadWait = TimeSpan.FromSeconds(int.Parse(ConfigurationManager.AppSettings["PageLoadTimeOut"]));
+
+            switch (ConfigurationManager.AppSettings["Browser"])
+            {
+                case "Chrome":
+                    var ChromeDriverService = OpenQA.Selenium.Chrome.ChromeDriverService.CreateDefaultService();
+                    ChromeDriverService.HideCommandPromptWindow = true;
+                    var chromeOption = new ChromeOptions
+                    {
+                        PageLoadStrategy = PageLoadStrategy.Normal,
+                    };
+                    chromeOption.AddArgument("--start-maximized");
+                    WrappedDriver = new ChromeDriver(ChromeDriverService, chromeOption, defaultExplicitWait);
+                    break;
+
+                case "Safari":
+                    break;
+
+                case "Firefox":
+                    break;
+            }
+
+            string url = ConfigurationManager.AppSettings["URL"];
+
+            WebDriverWaitInterval = new WebDriverWait(WrappedDriver, defaultExplicitWait);
+            WebDriverWaitInterval.IgnoreExceptionTypes(typeof(NoSuchElementException), typeof(ElementNotVisibleException,
+                typeof(StaleElementReferenceException), typeof(InvalidOperationException));
+
+            WrappedDriver.Manage().Timeouts().PageLoad = defaultPageLoadWait;
+            WrappedDriver.Manage().Window.Maximize();
+            WrappedDriver.SwitchTo().ActiveElement();
+
+            //Clearing Cookies
+            WrappedDriver.Manage().Cookies.DeleteAllCookies();
+        }
+
+        public static BaseDriver Instance => _instance ?? (_instance = new BaseDriver());
+        public IWebDriver WrappedDriver { get; }
+        public WebDriverWait WebDriverWaitInterval { get; }
+
+        public string Url { get => WrappedDriver.Url; set => WrappedDriver.Url = value; }
+        string IWebDriver.Url
+        {
+            get { return WrappedDriver.Url; }
+            set { WrappedDriver.Url = value; }
+        }
+
+        public string Title => WrappedDriver.Title;
+
+        public string PageSource => WrappedDriver.PageSource;
+
+        public string CurrentWindowHandle { get => WrappedDriver.CurrentWindowHandle; set => throw new NotImplementedException(); }
+
+        public ReadOnlyCollection<string> WindowHandles => WrappedDriver.WindowHandles;
+
+        public void Close()
+        {
+            WrappedDriver.Close();
+        }
+
+        public void Quit()
+        {
+            WrappedDriver.Quit();
+        }
+
+        public void Dispose()
+        {
+            WrappedDriver.Close();
+            WrappedDriver.Quit();
+            _instance = null;
+        }
+
+        public IOptions Manage()
+        {
+            return WrappedDriver.Manage();
+        }
+
+        public INavigation Navigate()
+        {
+            return WrappedDriver.Navigate();
+        }
+
+        public ITargetLocator SwitchTo()
+        {
+            return WrappedDriver.SwitchTo();
+        }
+
+        public IWebElement FindElement(By by)
+        {
+            return ((ISearchContext)_instance).FindElement(by);
+        }
+
+        public ReadOnlyCollection<IWebElement> FindElements(By by)
+        {
+            ReadOnlyCollection<IWebElement> elements = null;
+            DoUntil(() => { elements = WrappedDriver.FindElements(by); }); 
+            return ((ISearchContext)_instance).FindElements(by);
+        }
+
+        public void DoUntil(Action act)
+        {
+            try
+            {
+                WebDriverWaitInterval.Until(a =>
+                {
+                    act.Invoke();
+                    return true;
+                });
+            }
+            catch (Exception ex)
+            {
+                while(ex.InnerException != null)
+                {
+                    ex = ex.InnerException;
+                }
+                throw;
+            }
+        }
+    }
+}
---
+++ TestAutomationFramework_CWS/TAF_Util/ExtentExtention.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace TestAutomationFramework_CWS.TAF_Util
+{
+    public class ExtentExtention
+    {
+
+        public static readonly ExtentExtention _instance = new ExtentExtention();
+
+        private ExtentExtention() { }
+
+        public static ExtentExtention Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        //static constructor - to initialize report
+        static ExtentExtention()
+        {
+            var location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        }
+
+    }
+}
